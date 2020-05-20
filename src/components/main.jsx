40e4540c66aec3093d54b53e5827c373c1f9633c
Freddy Hernandez <freddy.hernandez@fandm.edu>
2020-05-20 19:17:55 -0400

    started structuring src folder with assets and components

---
+++ src/components/main.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+function Main() {
+  const [state, setState] = useState({});
+
+  useEffect(() => {})
+  
+  return (
+    <h1>hello world</h1>
+  )
+}
+
+export default Main;
