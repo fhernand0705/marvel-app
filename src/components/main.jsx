d99083de7c171b388bb9e181c1ca155b048ea269
Freddy Hernandez <freddy.hernandez@fandm.edu>
2020-05-21 19:52:33 -0400

    added character service to make request calls from external api

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,12 +1,28 @@
 import React, { useState, useEffect } from "react";
+import { getCharacters } from '../services/characters-service';
 
 function Main() {
-  const [state, setState] = useState({});
+  const [state, setState] = useState({
+    characters: []
+  });
 
-  useEffect(() => {})
-  
+  useEffect(() => {
+    fetchCharacters();
+  },[])
+
+  const fetchCharacters = async () => {
+    const { data } = await getCharacters()
+    console.log(data.results)
+
+    setState({...state, characters: data.results })
+  }
+
   return (
-    <h1>hello world</h1>
+    <div>
+      { state.characters.map(char =>
+        <div>{char.name}</div>
+      )}
+    </div>
   )
 }
 
