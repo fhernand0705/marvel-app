0eb91a76468f0bb994e12f80ad215309405528c2
Freddy Hernandez <freddy.hernandez@fandm.edu>
2020-06-19 17:43:57 -0400

    built navbar layout + styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import '../assets/main.scss';
+import app_logo from '../assets/images/app-logo.png';
 import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><NavLink to='/characters'>Characters</NavLink></li>
-        <li><NavLink to='/locations'>Locations</NavLink></li>
-      </ul>
-    </nav>
+    <div className="navbar-wrapper">
+      <nav className="navbar">
+        <ul className="navbar-items">
+          <li className="app-logo"><img src={app_logo} alt="App Logo"/></li>
+          <li className="app-title"><h4>Rick and Morty</h4></li>
+          <li className="nav-link"><NavLink to='/characters'>Characters</NavLink></li>
+          <li className="nav-link"><NavLink to='/locations'>Locations</NavLink></li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
