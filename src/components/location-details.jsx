a98d342e24f2f41457e1c49ee103de75dde21c09
Freddy Hernandez <freddy.hernandez@fandm.edu>
2020-07-07 17:04:21 -0400

    structured a grid layout for the locations component + styling

--- src/components/location-details.jsx
+++ src/components/location-details.jsx
@@ -3,14 +3,15 @@ import { NavLink } from 'react-router-dom';
 
 function LocationDetails({locations}) {
   return (
-    <div className="locations-wrapper">
+    <div className="locations-main-wrapper">
       { locations.map((location, i) =>
        location.name !== "unknown" && location.residents.length &&
-       <div className="location-card-wrapper" key={i}>
+       <div className="locations-section" key={i}>
             <h1>{location.name}</h1>
             <h5>Type: {location.type}</h5>
             <h5>Dimension: {location.dimension}</h5>
             <hr/>
+            <div className="location-cards-wrapper">
             { location.residents.map(res =>
                 <div className="location-card-content"  key={res.data.id}>
                   <img src={res.data.image} alt="Resident_Image"/>
@@ -19,6 +20,7 @@ function LocationDetails({locations}) {
                   </NavLink>
                 </div>
             )}
+            </div>
         </div>  
       )}
     </div>
